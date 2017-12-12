b8e64d27c4828157c9d18e1208d175b58102775d
Sonik S <gryazzz@gmail.com>
2017-12-12 02:20:21 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  name1: song1,
+  name2: song2,
+  name3: song3,
+  name4: song4
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistNamename = 'songTitle';
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
